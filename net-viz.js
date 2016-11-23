2429413421fbf72d7c597418d94b2ca1507488c4
Saima Sultana Tithi <saima.sultana.tithi@gmail.com>
2016-11-22 23:49:20 -0500

    working for input files

--- net-viz.js
+++ net-viz.js
@@ -1,118 +1,303 @@
-var width = 960,
-	height = 500,
-	radius = 6,
+var width = 960,     // svg width
+	height = 500,     // svg height
+	radius = 4,      // default node radius
+	offset = 15,    // cluster hull offset
 	padding = 5,
 	border = 1,
-	bordercolor = 'black';
+	bordercolor = 'black',
+	expand = {}, // expanded clusters
+	data, net, force, hullg, hull, linkg, link, nodeg, node;
+
+var curve = d3.svg.line()
+	.interpolate("cardinal-closed")
+	.tension(.85);
+
+var fill = d3.scale.category20();
+
+function noop() { return false; }
+
+function nodeid(n) {
+	return n.size ? "Group_" + n.group : n.name;
+}
+
+function linkid(l) {
+	var u = nodeid(l.source),
+	v = nodeid(l.target);
+	return u<v ? u+"|"+v : v+"|"+u;
+}
+
+function getGroup(n) { 
+	/* if (n.group == "DSE")
+		return 1;
+	else if (n.group == "SRH")
+		return 2;
+	else if (n.group == "DMCT")
+		return 3;
+	else if (n.group == "SFLE")
+		return 4;
+	else if (n.group == "DISQ")
+		return 5; */
+	return n.group;
+}
+
+// constructs the network to visualize
+function network(data, prev, expand) {
+	expand = expand || {};
+	var gm = {},    // group map
+	nm = {},    // node map
+	lm = {},    // link map
+	gn = {},    // previous group nodes
+	gc = {},    // previous group centroids
+	nodes = [], // output nodes
+	links = []; // output links
+
+	// process previous nodes for reuse or centroid calculation
+	if (prev) {
+		prev.nodes.forEach(function(n) {
+			var i = getGroup(n), o;
+			if (n.size > 0) {
+				gn[i] = n;
+				n.size = 0;
+			} else {
+				o = gc[i] || (gc[i] = {x:0,y:0,count:0});
+				o.x += n.x;
+				o.y += n.y;
+				o.count += 1;
+			}
+		});
+	}
+
+	// determine nodes
+	for (var k=0; k<data.nodes.length; ++k) {
+		var n = data.nodes[k];
+		var i = getGroup(n),
+		l = gm[i] || (gm[i]=gn[i]) || (gm[i]={group:i, size:0, nodes:[]});
+
+		if (expand[i]) {
+			// the node should be directly visible
+			nm[n.name] = nodes.length;
+			nodes.push(n);
+		if (gn[i]) {
+			// place new nodes at cluster location (plus jitter)
+			n.x = gn[i].x + Math.random();
+			n.y = gn[i].y + Math.random();
+		}
+		} else {
+			// the node is part of a collapsed cluster
+			if (l.size == 0) {
+				// if new cluster, add to set and position at centroid of leaf nodes
+				nm[i] = nodes.length;
+				nodes.push(l);
+				if (gc[i]) {
+					l.x = gc[i].x / gc[i].count;
+					l.y = gc[i].y / gc[i].count;
+				}
+			}
+			l.nodes.push(n);
+		}
+		// always count group size as we also use it to tweak the force graph strengths/distances
+		l.size += 1;
+		n.group_data = l;
+	}
+
+	for (i in gm) { gm[i].link_count = 0; }
+
+	// determine links
+	for (k=0; k<data.links.length; ++k) {
+		var e = data.links[k];
+		var u = getGroup(e.source);
+		var v = getGroup(e.target);
+		if (u != v) {
+			gm[u].link_count++;
+			gm[v].link_count++;
+		}
+		u = expand[u] ? nm[e.source.name] : nm[u];
+		v = expand[v] ? nm[e.target.name] : nm[v];
+		var i = (u<v ? u+"|"+v : v+"|"+u),
+		l = lm[i] || (lm[i] = {source:u, target:v, size:0});
+		l.size += 1;
+	}
+	
+	for (i in lm) { links.push(lm[i]); }
+
+	return {nodes: nodes, links: links};
+}
+
+function convexHulls(nodes, offset) {
+	var hulls = {};
+
+	// create point sets
+	for (var k=0; k<nodes.length; ++k) {
+		var n = nodes[k];
+		if (n.size) 
+			continue;
+		
+		var i = getGroup(n);
+		
+		var l = hulls[i] || (hulls[i] = []);
+		l.push([n.x-offset, n.y-offset]);
+		l.push([n.x-offset, n.y+offset]);
+		l.push([n.x+offset, n.y-offset]);
+		l.push([n.x+offset, n.y+offset]);
+	}
+
+	// create convex hulls
+	var hullset = [];
+	for (i in hulls) {
+		hullset.push({group: i, path: d3.geom.hull(hulls[i])});
+	}
+
+	return hullset;
+}
+
+function drawCluster(d) {
+	return curve(d.path); // 0.8
+}
+
+// --------------------------------------------------------
 
 var forceGraph = d3.select("#force-layout-graph").append("svg")
-.attr("width", width)
-.attr("height", height)
-.attr("border",border);
+	.attr("width", width)
+	.attr("height", height)
+	.attr("border",border);
 
 var borderPath = forceGraph.append("rect")
-			.attr("x", 0)
-			.attr("y", 0)
-			.attr("height", height)
-			.attr("width", width)
-			.style("stroke", bordercolor)
-			.style("fill", "none")
-			.style("stroke-width", border);
+	.attr("x", 0)
+	.attr("y", 0)
+	.attr("height", height)
+	.attr("width", width)
+	.style("stroke", bordercolor)
+	.style("fill", "none")
+	.style("stroke-width", border);	
 
-var colorArr = d3.schemeCategory20;
-
-d3.json("miserables.json", function(error, graph) {
+d3.json("data/workplace_small.json", function(error, json) {
 	if (error) throw error;
-	var links = forceGraph.append("g")
-		.attr("class", "links")
-		.selectAll("line")
-		.data(graph.links)
-		.enter()
-		.append("line")
-		.attr("stroke-width", function(d) {
-			return Math.sqrt(d.value);
-		})
-		.attr("x1", 0)
-		.attr("y1", 0)
-		.attr("x2", width)
-		.attr("y2", height);
-	
-	var nodes = forceGraph.append("g")
-		.attr("class", "nodes")
-		.selectAll("circle")
-		.data(graph.nodes)
-		.enter()
-		.append("circle")
-		.attr("fill", function(d) {
-			return colorArr[d.group];
-		})
-		.attr("r", radius)
-		.attr("cx", function(d) {
-			return Math.random() * width;
-			})
-		.attr("cy", function(d) {
-			return Math.random() * height;
-			})
-		.call(d3.drag()
-				.on("start", started)
-				.on("drag", dragged)
-				.on("end", ended));
-				
-	nodes.append("title")
-      		.text(function(d) { return d.id; });
-	
-	simulation
-		.nodes(graph.nodes)
-		.on("tick", ticked);
-
-	simulation.force("link")
-		.links(graph.links);
-		
-	function ticked(d) {
-	nodes
-		.attr("cx", function(d) {
-			return d.x;
-		})
-		.attr("cy", function(d) {
-			return d.y;
-		})
-	
-	links 
-		.attr("x1", function (d) {
-			return d.source.x;
-		})
-		.attr("y1", function (d) {
-			return d.source.y;
-		})
-		.attr("x2", function (d) {
-			return d.target.x;
-		})
-		.attr("y2", function (d) {
-			return d.target.y;
-		})
+	data = json;
+	for (var i=0; i<data.links.length; ++i) {
+		o = data.links[i];
+		var sourceNode, targetNode;
+		for (var k = 0; k < data.nodes.length; ++k) {
+			var node = data.nodes[k];
+			if (o.source === node.name) {
+				sourceNode = node;
+				continue;
+			}
+			if (o.target === node.name) {
+				targetNode = node;
+				continue;
+			}
+		}
+		o.source = sourceNode;
+		o.target = targetNode;
 	}
-		
+
+	hullg = forceGraph.append("g");
+	linkg = forceGraph.append("g");
+	nodeg = forceGraph.append("g");
+
+	init();
+
+	forceGraph.attr("opacity", 1e-6)
+		.transition()
+		.duration(1000)
+		.attr("opacity", 1);
 });
 
-function started(d) {
-	simulation.alphaTarget(0.3).restart();
+function init() {
+	if (force) force.stop();
+
+	net = network(data, net, expand);
+
+	force = d3.layout.force()
+		.nodes(net.nodes)
+		.links(net.links)
+		.size([width, height])
+		.linkDistance(function(l, i) {
+			var n1 = l.source, n2 = l.target;
+			// larger distance for bigger groups:
+			// both between single nodes and _other_ groups (where size of own node group still counts),
+			// and between two group nodes.
+			//
+			// reduce distance for groups with very few outer links,
+			// again both in expanded and grouped form, i.e. between individual nodes of a group and
+			// nodes of another group or other group node or between two group nodes.
+			//
+			// The latter was done to keep the single-link groups ('blue', rose, ...) close.
+			return 30 +
+				Math.min(20 * Math.min((n1.size || (n1.group != n2.group ? n1.group_data.size : 0)),
+					(n2.size || (n1.group != n2.group ? n2.group_data.size : 0))),
+					-30 +
+					30 * Math.min((n1.link_count || (n1.group != n2.group ? n1.group_data.link_count : 0)),
+					(n2.link_count || (n1.group != n2.group ? n2.group_data.link_count : 0))),
+					100);
+			//return 150;
+		})
+		.linkStrength(function(l, i) {
+			return 1;
+		})
+		.gravity(0.05)   // gravity+charge tweaked to ensure good 'grouped' view (e.g. green group not smack between blue&orange, ...
+		.charge(-600)    // ... charge is important to turn single-linked groups to the outside
+		.friction(0.8)   // friction adjusted to get dampened display: less bouncy bouncy ball [Swedish Chef, anyone?]
+		.on("tick", ticked)
+		.start();
+
+	hullg.selectAll("path.hull").remove();
+	hull = hullg.selectAll("path.hull")
+		.data(convexHulls(net.nodes, offset))
+		.enter().append("path")
+		.attr("class", "hull")
+		.attr("d", drawCluster)
+		.style("fill", function(d) { return fill(d.group); })
+		.on("click", function(d) {
+			console.log("hull click", d, arguments, this, expand[d.group]);
+			expand[d.group] = false; 
+			init();
+		});
+
+	link = linkg.selectAll("line.link").data(net.links, linkid);
+	link.exit().remove();
+	link.enter().append("line")
+		.attr("class", "link")
+		.attr("x1", function(d) { return d.source.x; })
+		.attr("y1", function(d) { return d.source.y; })
+		.attr("x2", function(d) { return d.target.x; })
+		.attr("y2", function(d) { return d.target.y; })
+		.style("stroke-width", function(d) { return d.size * 0.25 || 1; });
+		//.style("stroke-width", function(d) { return 1; });
+
+	node = nodeg.selectAll("circle.node").data(net.nodes, nodeid);
+	node.exit().remove();
+	node.enter().append("circle")
+		// if (d.size) -- d.size > 0 when d is a group node.
+		.attr("class", function(d) { return "node" + (d.size?"":" leaf"); })
+		.attr("r", function(d) { return d.size ? d.size + radius : radius+1; })
+		.attr("cx", function(d) { return d.x; })
+		.attr("cy", function(d) { return d.y; })
+		.style("fill", function(d) { return fill(d.group); })
+		.style("stroke", function(d) { return d3.rgb(fill(d.group)).darker(); })
+		.on("click", function(d) {
+			console.log("node click", d, arguments, this, expand[d.group]);
+			expand[d.group] = !expand[d.group];
+			init();
+		});
+
+	node.append("title")
+		.text(function(d) { return nodeid(d) });
+	
+	node.call(force.drag);
+
+	function ticked(d) {
+		if (!hull.empty()) {
+			hull.data(convexHulls(net.nodes, offset))
+				.attr("d", drawCluster);
+		}
+
+		link.attr("x1", function(d) { return d.source.x; })
+			.attr("y1", function(d) { return d.source.y; })
+			.attr("x2", function(d) { return d.target.x; })
+			.attr("y2", function(d) { return d.target.y; });
+
+		node.attr("cx", function(d) { return d.x = Math.max(radius, Math.min(width - radius, d.x)); })
+			.attr("cy", function(d) { return d.y = Math.max(radius, Math.min(height - radius, d.y)); });
+	}
 }
-
-function dragged(d) {
-	d.fx = d3.event.x;
-	d.fy = d3.event.y;
-}
-
-function ended(d) {
-	simulation.alphaTarget(0);
-	d.fx = null;
-	d.fy = null;		
-}
-
-var simulation = d3.forceSimulation()
-				.force("link", d3.forceLink().id(function (d) {
-					return d.id;
-				}))
-				.force("repulse", d3.forceManyBody().strength(-30))
-				.force("center", d3.forceCenter(width/2, height/2));
-
